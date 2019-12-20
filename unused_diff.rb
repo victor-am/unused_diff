58d722210c95bfb310c77d025d23800a7f629d5e
Victor Marques <victor.atmorning@gmail.com>
2019-12-20 18:26:25 -0300

    Fix bug on skip-master

--- unused_diff.rb
+++ unused_diff.rb
@@ -1,5 +1,11 @@
+#!/usr/bin/env ruby
+
+require 'fileutils'
+
 MASTER_UNUSED_PATH = '../unused.master'.freeze
 BRANCH_UNUSED_PATH = '../unused.branch'.freeze
+MASTER_UNUSED_TMP_PATH = '../unused.master.tmp'.freeze
+BRANCH_UNUSED_TMP_PATH = '../unused.branch.tmp'.freeze
 
 # Unused Diff
 # -----------
@@ -18,10 +24,10 @@ BRANCH_UNUSED_PATH = '../unused.branch'.freeze
 # How to use:
 # -----------
 # > git checkout <branch you want to compare with master>
-# > ruby unused_diff.rb
+# > ./unused_diff.rb
 #
 # Another option is to run
-# > ruby unused_diff.rb --skip-master
+# > ./unused_diff.rb --skip-master
 #
 # This will run the check without generating new unused analysis for the master
 # it should be useful if you already performed the fixes and want to re-run the
@@ -29,17 +35,24 @@ BRANCH_UNUSED_PATH = '../unused.branch'.freeze
 
 puts ''
 unless %w[--skip-master].include?(ARGV[0])
-  File.delete(MASTER_UNUSED_PATH) if File.exist?(MASTER_UNUSED_PATH)
   system 'git checkout master'
   puts 'Analyzing master unused code...'
-  system "git ls-files | grep .rb | xargs ctags -f tmp/tags && cat tmp/tags | unused -s -g none --stdin -C > #{MASTER_UNUSED_PATH}"
+  system "git ls-files | grep .rb | xargs ctags -f tmp/tags && cat tmp/tags | unused -s -g none --stdin -C > #{MASTER_UNUSED_TMP_PATH}"
+
+  File.delete(MASTER_UNUSED_PATH) if File.exist?(MASTER_UNUSED_PATH)
+  FileUtils.cp(MASTER_UNUSED_TMP_PATH, MASTER_UNUSED_PATH)
+  File.delete(MASTER_UNUSED_TMP_PATH)
+
+  system 'git checkout -'
 end
 
 unless %w[--skip-branch].include?(ARGV[0])
-  File.delete(BRANCH_UNUSED_PATH) if File.exist?(BRANCH_UNUSED_PATH)
-  system 'git checkout -'
   puts 'Analyzing branch unused code...'
-  system "git ls-files | grep .rb | xargs ctags -f tmp/tags && cat tmp/tags | unused -s -g none --stdin -C > #{BRANCH_UNUSED_PATH}"
+  system "git ls-files | grep .rb | xargs ctags -f tmp/tags && cat tmp/tags | unused -s -g none --stdin -C > #{BRANCH_UNUSED_PATH}.tmp"
+
+  File.delete(BRANCH_UNUSED_PATH) if File.exist?(BRANCH_UNUSED_PATH)
+  FileUtils.cp(BRANCH_UNUSED_TMP_PATH, BRANCH_UNUSED_PATH)
+  File.delete(BRANCH_UNUSED_TMP_PATH)
 end
 
 def clean_output(file_content)
